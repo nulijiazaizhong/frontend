60c63a2cbb37867aa1e851bba648ea14f8041a84
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2025-01-30 19:12:51 +0200

    Fade visualization and map to each other

--- app/visualization/page.tsx
+++ app/visualization/page.tsx
@@ -16,7 +16,14 @@ export default function Visualization() {
     return (
         <motion.div className="flex w-full h-full">
             <ResizablePanelGroup direction="horizontal" className="w-full h-full">
-                <ResizablePanel className="h-full" defaultSize={40}>
+                <ResizablePanel className="h-full relative" defaultSize={40} onResize={(size) => {
+                    if(size < 5){
+                        setIsVisualizationOpen(false);
+                    }
+                }}>
+                    {isMapOpen && isVisualizationOpen && (
+                        <div className="absolute right-0 top-0 bottom-0 w-4 z-10 bg-gradient-to-r from-transparent to-[#181818]" />
+                    )}
                     {isVisualizationOpen && (
                         <motion.iframe className="w-full h-full" 
                             initial={{ opacity: 0 }}
@@ -41,8 +48,15 @@ export default function Visualization() {
                         </div>
                     )}
                 </ResizablePanel>
-                <ResizableHandle withHandle className="bg-transparent" />
-                <ResizablePanel className="h-full w-0" defaultSize={60}>
+                <ResizableHandle withHandle className="bg-transparent w-0 opacity-20 hover:opacity-100 z-50 transition-all" />
+                <ResizablePanel className="h-full w-0 relative" defaultSize={60} onResize={(size) => {
+                    if(size < 5){
+                        setIsMapOpen(false);
+                    }
+                }}>
+                    {isVisualizationOpen && isMapOpen && (
+                        <div className="absolute left-0 top-0 bottom-0 w-24 z-10 bg-gradient-to-l from-transparent to-[#181818]" />
+                    )}
                     {isMapOpen && (
                         <motion.iframe className="w-full h-full invert hue-rotate-180 saturate-[0.8] brightness-[0.83] contrast-[1.3]" 
                             initial={{ opacity: 0 }}
