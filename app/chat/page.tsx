3722040de790cb33d9bc865e0ed19ec615a3484f
DylDev <dylanb92010@gmail.com>
2025-06-11 10:32:58 -0400

    Revert "Remove current /chat TSX page"
    This reverts commit bb66078ff307ef9e0ea9cb85918476f0bc6aef7b.

---
+++ app/chat/page.tsx
@@ -0,0 +1,41 @@
+"use client"
+import RenderPage from "@/components/page/render_page";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+import { Button } from "@/components/ui/button"
+
+export default function Updater() {
+    const { setTheme } = useTheme();
+
+    return (
+        <div className="w-full h-full overflow-auto">
+            <DropdownMenu>
+                <DropdownMenuTrigger className="absolute right-2 bottom-2 z-50">
+                    <Button variant="outline" size="icon">
+                    <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <span className="sr-only">Toggle theme</span>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="bg-input/30 font-geist">
+                    <DropdownMenuItem onClick={() => setTheme("light")}>
+                        Light
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("dark")}>
+                        Dark
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("system")}>
+                        System
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <RenderPage url="/chat" className="max-w-full w-full" />
+        </div>
+    )
+}
