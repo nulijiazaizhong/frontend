469f19dcdb7e4e5fabd047c7af4e4b0b0da68206
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2025-07-15 19:47:34 +0300

    Add back fallback for when ETS2LA was not found.

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({ children, } : Readonly<{ children: React.Re
                 <meta name="theme-color" content="#131316" />
                 <meta name="google-adsense-account" content="ca-pub-6002744323117854" />
                 <link rel="icon" href="/favicon.ico" />
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6002744323117854" crossOrigin="anonymous"></script>
             </head>
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-sidebarbg overflow-hidden`}>
                 <Script src="https://umami.ets2la.com/script.js" data-website-id="ca602362-299b-4222-9ea5-bbd2610488b3" onError={() => {}} />
