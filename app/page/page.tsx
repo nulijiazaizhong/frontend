4489306f75933ff764a3d1d32c6805d0fa376829
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2025-09-19 14:47:54 +0300

    Add special handling for manually opened settings pages.

--- app/page/page.tsx
+++ app/page/page.tsx
@@ -7,6 +7,16 @@ export default function Page() {
     const searchParams = useSearchParams()
     const path = searchParams.get("url") || "/"  // Default to "/" if no URL is provided
 
+    // If we are getting a settings page but we are not in the settings
+    // then we want to render it inside a padded container.
+    // This will make sure that manually opened settings pages are 
+    // rendered at least somewhat nicely.
+    if (!window.location.origin.includes("settings") && path.startsWith("/settings")) {
+        return <div className="w-full h-full p-16">
+            <RenderPage url={path} className="max-w-full w-full" />
+        </div>
+    }
+
     // Render the page based on the URL
     return <RenderPage url={path} className="max-w-full w-full" />
 }
